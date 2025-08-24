b223fff1d6e42a64be2318692e4db2cd7bc9e94e
vicct0r <v1ctor_inacio@proton.me>
2025-08-24 09:31:39 -0300

    new: module test for CD endpoints

---
+++ cd/tests/test_endpoints.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+import requests
+
+class TestCD(TestCase):
+    url_base_cd = "http://192.168.1.83:8000/cd/v1/"
+
+    def test_post_product(self):
+        data = {
+            "name": "orange_pi",
+            "quantity": 12,
+            "price": 20.00
+        }
+        
+        response = requests.post(url=f"{self.url_base_cd}product/register/", data=data, timeout=5)
+        assert response.status_code == 201
+
+    def test_get_product_detail(self):
+        response = requests.get(url=f"{self.url_base_cd}info/raspberry/")
+        assert response.json()['name'] == "orange_pi" and requests.json()['quantity'] == 12
+
+    def test_request_cd(self):
+        response = requests.get(url=f"{self.url_base_cd}product/request/raspberry/13/")
+        assert response.json()['available'] == False and response.status_code == 200
+
+    def test_sell_endpoint(self):
+        response = requests.post(url=f"{self.url_base_cd}product/sell/raspberry/10/", timeout=5)
+        assert response.status_code == 200
